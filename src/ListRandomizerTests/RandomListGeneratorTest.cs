dd81368028d3b360a12707fc05b13ed0317b3426
yadian <yadianfdez07@gmail.com>
2022-07-03 10:36:50 -0400

    add list randomizer test project

---
+++ src/ListRandomizerTests/RandomListGeneratorTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace ListRandomizerTests
+{
+    public class RandomListGeneratorTest
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            // arrange
+
+
+            // act
+
+            // assert
+            Assert.True(1 == 1);
+        }
+    }
+}
