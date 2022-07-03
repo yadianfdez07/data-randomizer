c9425c63d5b1e0ba13b096507beb52e0b066e5e7
yadian <yadianfdez07@gmail.com>
2022-07-03 14:07:06 -0400

    add implementation to generate random list of int

--- src/ListRandomizer/RandomListGenerator.cs
+++ src/ListRandomizer/RandomListGenerator.cs
@@ -1,9 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
 
 namespace ListRandomizer
 {
     public class RandomListGenerator
     {
-        public static List<int> GetRandomIntList() { return new List<int>() { 1, 2 }; }
+        public static List<int> GetRandomIntList()
+        {
+            var randomList = new List<int>();
+
+            var listSize = GetRandomInt(1, 10);
+
+            for (int i = 0; i < listSize; i++)
+            {
+                randomList.Add(GetRandomInt());
+            }
+
+            return randomList;
+        }
+
+
+        private static int GetRandomInt(int min = int.MinValue, int max = int.MaxValue)
+        {
+            var pingSender = new Ping();
+            var options = new PingOptions() { DontFragment = true };
+
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send("8.8.4.4", timeout, buffer, options);
+
+            var randomizer = new Random((int)(reply.RoundtripTime * DateTime.Now.Millisecond * DateTime.Now.Ticks));
+
+            var randomIntValue = randomizer.Next(min, max);
+
+            return randomIntValue;
+        }
     }
 }
--- src/ListRandomizerTests/RandomListGeneratorTest.cs
+++ src/ListRandomizerTests/RandomListGeneratorTest.cs
@@ -1,4 +1,6 @@
 ﻿using ListRandomizer;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace ListRandomizerTests
@@ -15,6 +17,53 @@ namespace ListRandomizerTests
 
             // assert
             Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public void ShouldReturnRandomListOfIntEveryTime()
+        {
+            // arrange
+            var resultingLists = new List<List<int>>();
+            var iterations = 5;
+
+            // act
+            for (int i = 0; i < iterations; i++)
+            {
+                resultingLists.Add(RandomListGenerator.GetRandomIntList());
+            }
+
+            // assert
+            Assert.False(ContainsEqualList(resultingLists));
+            Assert.NotEqual(resultingLists[0], resultingLists[1]);
+            Assert.NotEqual(resultingLists[0], resultingLists[2]);
+            Assert.NotEqual(resultingLists[0], resultingLists[3]);
+            Assert.NotEqual(resultingLists[0], resultingLists[4]);
+        }
+
+        private bool ContainsEqualList(List<List<int>> listsToCompare)
+        {
+
+            foreach (var list in listsToCompare)
+            {
+                var otherLists = listsToCompare.Where(l => !list.Equals(l)).Select(l => l.ToList()).ToList();
+
+                foreach (var otherList in otherLists)
+                {
+                    foreach (var item in list)
+                    {
+                        foreach (var target in otherList)
+                        {
+                            if (item == target)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            return false;
         }
     }
 }
